943a40252975b16cf2974cbfee19606e29ae09b3
Rohit Nagane <rohit.nagane@tcs.com>
2016-10-04 10:47:54 +0530

    UAA issue resolved

--- ehs-ui/web/public/scripts/routes.js
+++ ehs-ui/web/public/scripts/routes.js
@@ -13,15 +13,13 @@ define([ 'angular', 'angular-ui-router' ], function(angular) {
 		 * Router paths This is where the name of the route is matched to the
 		 * controller and view template.
 		 */
-		$stateProvider
-		/*.state('secure', {
+		$stateProvider.state('secure', {
 			template : '<ui-view/>',
 			abstract : true,
 			resolve : {
 				authenticated : [ '$q', 'PredixUserService', function($q, predixUserService) {
 					var deferred = $q.defer();
 					predixUserService.isAuthenticated().then(function(userInfo) {
-						// console.log(userInfo);
 						deferred.resolve(userInfo);
 					}, function() {
 						deferred.reject({
@@ -31,14 +29,11 @@ define([ 'angular', 'angular-ui-router' ], function(angular) {
 					return deferred.promise;
 				} ]
 			}
-		})*/
-			.state('dashboards', {
-			//parent : 'secure',
+		}).state('dashboards', {
+			parent : 'secure',
 			url : '/dashboards',
 			templateUrl : 'views/dashboards-floor.html',
-			controller : 'DashboardsCtrlFloor',
-		// templateUrl : 'views/dashboards.html',
-		// controller : 'DashboardsCtrl',
+			controller : 'DashboardsCtrlFloor'
 		})
 
 		.state('asset_detail', {
@@ -50,14 +45,14 @@ define([ 'angular', 'angular-ui-router' ], function(angular) {
 			templateUrl : 'views/garph_demo.html',
 			controller : 'GraphCtrl'
 		}).state('airquality', {
-			// url : '/airquality',
-			// templateUrl : 'views/airquality.html',
 			url : '/airquality',
 			templateUrl : 'views/aqi-floor.html',
 			controller : 'AqiController',
 			params : {
 				'smtare' : null
 			}
+		  
+		 
 		}).state('detail_parameter', {
 			url : '/detail_parameter',
 			templateUrl : 'views/detail_parameter.html',
@@ -71,6 +66,7 @@ define([ 'angular', 'angular-ui-router' ], function(angular) {
 				'type' : null,
 				'assetName' : null
 			}
+		
 		}).state('hygiene-details', {
 			url : '/hygiene-details',
 			templateUrl : 'views/hygiene-details-page.html',
@@ -81,20 +77,20 @@ define([ 'angular', 'angular-ui-router' ], function(angular) {
 			url : '/detilgraph',
 			templateUrl : 'views/detilgraph.html',
 			controller : 'detilgraphCtrl'
-		}).state('industrial-hygiene', {
-			url : '/industrial-hygiene',
-			templateUrl : 'views/industrial-hygiene.html',
-			controller : 'industrialHygiene'
-		})/*.state('industrial-hygiene-details', {
-			url : '/industrial-hygiene-details',
-			templateUrl : 'views/industrial-hygiene-details.html',
-			controller : 'industrialHygieneDetails'
-		})*/
+		})
 
 		.state('blanksubpage', {
 			url : '/blanksubpage',
 			templateUrl : 'views/blank-sub-page.html'
-		});
+		}).state('industrial-hygiene', {
+			url : '/industrial-hygiene',
+			templateUrl : 'views/industrial-hygiene.html',
+			controller : 'industrialHygiene'
+		}).state('industrial-hygiene-details', {
+			url : '/industrial-hygiene-details',
+			templateUrl : 'views/industrial-hygiene-details.html',
+			controller : 'industrialHygieneDetails'
+		})
 
 		$urlRouterProvider.otherwise(function($injector) {
 			var $state = $injector.get('$state');
